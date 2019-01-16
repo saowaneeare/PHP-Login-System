a621bef7092ef9df92290bd96b84ecf65ef58507
saowaneeare <saowaneeare@gmail.com>
2019-01-16 17:32:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+	
+	define('CONFIG', true);
+	require_once "inc/config.php"; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <html>
@@ -13,39 +18,20 @@
 <body>	
 
 	<div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-		<form class="uk-form-stacked js.login">
 
-    <div class="uk-margin">
-        <label class="uk-form-label" for="form-stacked-text">Email</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="Email@email.com">
-        </div>
-    </div>
+		<?php 
+			echo "Hell World Today is :";
+			echo date ("y m d");
+		?>
+		<p>
+			<a href="login.php"> Login </a>
+			<a href="register.php"> Register </a>
 
-     <div class="uk-margin">
-        <label class="uk-form-label" for="form-stacked-text">Password</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-        </div>
-    </div>
-
-    <div class="uk-margin">
-    	<button class="uk-button uk-button-default" type="submit">Login</button>
-    </div>
-
-    
-   
-</form>
-		</div>
 	</div>
 
-	<!-- JQuery is required 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	-->
+<?php require_once "inc/header.php"; ?>
 
-	<!-- UIkit JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit-icons.min.js"></script>
-
+	
 </body>
+
+</html>
