7a98abc483b742ac18585144fa489dc6a52bc226
saowaneeare <saowaneeare@gmail.com>
2019-01-16 17:33:20 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+	<!-- JQuery is required -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	
+
+	<!-- UIkit JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit-icons.min.js"></script>
